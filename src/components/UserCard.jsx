dac0ea2b2c5e7ba2cd0adc305457fd86ab1e38e5
Fabricio Rezende <fabriciorez@gmail.com>
2025-09-04 08:53:03 -0300

    Inserido o botão de editar

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,5 +1,5 @@
-const UserCard = ({ usuario, onDelete }) => {
-  const { nome, endereco, email, telefone } = usuario;
+const UserCard = ({ usuario, onDelete, onUpdate }) => {
+  const { id, nome, endereco, email, telefone } = usuario;
 
   return (
     <tr>
@@ -8,11 +8,22 @@ const UserCard = ({ usuario, onDelete }) => {
       <td>{email}</td>
       <td>{telefone}</td>
       <td className="text-center align-middle">
+        {/* Botão Editar */}
+        <button
+          className="btn border-0 btn-sm me-2"
+          onClick={() => onUpdate(usuario)}
+          title={`Editar ${nome}`}
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+        >
+          <i className="bi bi-pencil-square"></i>
+        </button>
+
+        {/* Botão Excluir */}
         <button
           className="btn border-0 btn-sm"
-          onClick={onDelete}
+          onClick={() => onDelete(id)}
           title={`Excluir ${nome}`}
-          aria-label={`Excluir usuário ${nome}`}
           data-bs-toggle="tooltip"
           data-bs-placement="top"
         >
@@ -24,3 +35,4 @@ const UserCard = ({ usuario, onDelete }) => {
 };
 
 export default UserCard;
+
